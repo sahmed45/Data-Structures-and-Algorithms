4d5aa1eae45844c302144bb0f9bf047d41b5c5a2
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-01-02 19:15:54 -0500

    Update quick_sort.py

--- Algorithms/quick_sort.py
+++ Algorithms/quick_sort.py
@@ -3,8 +3,8 @@ def quick_sort(data: list) -> list:
     if len(data) < 2:
         return data
     pivot = data.pop()  # Use the last element as the first pivot
-    greater: list[int] = []  # All elements greater than pivot
-    lesser: list[int] = []  # All elements less than or equal to pivot
+    greater = []  # All elements greater than pivot
+    lesser = []  # All elements less than or equal to pivot
     for element in data:
         (greater if element > pivot else lesser).append(element)
     return quick_sort(lesser) + [pivot] + quick_sort(greater)
