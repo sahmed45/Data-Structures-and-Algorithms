39fdd8fc4553ff5c48a40a3757d7092f7543f9d3
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-01-02 19:08:00 -0500

    Create recursive_quick_sort.py

---
+++ Algorithms/recursive_quick_sort.py
@@ -0,0 +1,10 @@
+def quick_sort(data: list) -> list:
+
+    if len(data) <= 1:
+        return data
+    else:
+        return (
+            quick_sort([e for e in data[1:] if e <= data[0]])
+            + [data[0]]
+            + quick_sort([e for e in data[1:] if e > data[0]])
+        )
