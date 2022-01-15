59c74b9c640758f6ddac7e07c71cf9465793b693
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-01-15 12:53:45 -0500

    Time: 1100 ms (19.14%), Space: 27.5 MB (76.35%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -2,7 +2,6 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         l, r = 0, len(height) - 1
         res = 0
-        minHeight = math.inf
         while l < r:
             minHeight = min(height[l], height[r])
             area = minHeight * (r - l)
