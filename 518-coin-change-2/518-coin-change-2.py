4d3b7c5de701c96a5fe7be5fd73919253adaa089
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-01-16 15:50:50 -0500

    Time: 197 ms (68.44%), Space: 14.4 MB (86.13%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -7,17 +7,11 @@ class Solution:
         
         dp = [0] * (amount + 1)
         dp[0] = 1
-        for i in range(len(coins) - 1, -1, -1):
-            nextDP = [0] * (amount + 1)
-            nextDP[0] = 1
-            
-            for a in range(1, amount + 1):
-                nextDP[a] =  dp[a]
-                if a - coins[i] >= 0:
-                    nextDP[a] += nextDP[a - coins[i]] 
-            dp = nextDP
-        return dp[amount]        
         
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] += dp[x - coin]
+        return dp[amount]
         
         
         
