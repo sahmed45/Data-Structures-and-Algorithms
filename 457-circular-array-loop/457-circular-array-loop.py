04fc5e2d78640715a93f0d344db97cadee0183da
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-01-20 21:33:21 -0500

    Time: 35 ms (81.89%), Space: 14.4 MB (26.63%) - LeetHub

--- 457-circular-array-loop/457-circular-array-loop.py
+++ 457-circular-array-loop/457-circular-array-loop.py
@@ -1,14 +1,35 @@
 class Solution:
-    def circularArrayLoop(self, nums: List[int]) -> bool:        
-    	s, l = set(), len(nums)
-    	for i, n in enumerate(nums):
-    		if i in s: continue                 
-    		d,j,last = set(),i,i
-    		while n*nums[j]>0:               
-    			if j in d:
-    				if last!=j: return True 
-    				else: break
-    			d.add(j)
-    			s.add(j)                     
-    			j,last = (j+nums[j])%l,j
-    	return False
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n == 1: return False # easy case
+        
+        def getNext(ix): #next index
+            return (ix + nums[ix]) % n
+        
+        def checkCycle(ix): #check if cycle lenght>1 and all element all positive/negative 
+            visited[ix]=True
+            if ix == getNext(ix):
+                return False
+            
+            temp = getNext(ix)
+            while temp != ix:
+                visited[temp]=True
+                if nums[temp]*nums[ix]<0:
+                    return False
+                temp = getNext(temp)
+            return True
+            
+        visited = [False]*n
+
+        for i in range(n):
+            if visited[i]: continue
+            s = i #slow pointer
+            f = i #fast pointer
+            while not visited[s]:
+                visited[s]=True
+                s = getNext(s)
+                f = getNext(getNext(f))
+                if s == f and checkCycle(s):
+                    return True
+ 
+        return False
