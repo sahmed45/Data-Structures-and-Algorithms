3fc75f5b4c5cb0591348bbf6396d13342f674c22
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-01-21 18:52:08 -0500

    Time: 51 ms (23.61%), Space: 14.2 MB (52.82%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -6,39 +6,17 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-        if not root:
-            return res
+        if not root: return root
         
-#         def dfs(node,level):
-
-            
-#             if level >= len(res):
-#                 res.append(node.val)
-            
-#             if node.right:
-#                 dfs(node.right,level + 1)
-            
-#             if node.left:
-#                 dfs(node.left, level + 1)
-                
-#         dfs(root, 0)
-#         return res
-
-        q = deque()
-        q.append(root)
+        q = deque([root])
+        res = []
         
         while q:
-            level = len(q)
-            
-            for i in range(level):
+            res.append(q[0].val)
+            for _ in range(len(q)):
                 node = q.popleft()
-                #if its the right most
-                if i == level - 1:
-                    res.append(node.val)
-                if node.left:
-                    q.append(node.left)
                 if node.right:
                     q.append(node.right)
+                if node.left:
+                    q.append(node.left)
         return res
-    
