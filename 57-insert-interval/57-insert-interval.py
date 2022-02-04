c033c493fcd91e01f737e46d7e088b3812b8bc2f
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-02-04 18:17:51 -0500

    Time: 180 ms (5.08%), Space: 17.2 MB (98.85%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -2,10 +2,12 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         output = []
         
-        for idx in range(len(intervals)):  
+        for idx in range(len(intervals)):
+            #if previous intervals start after new one ends, just append new and return rest of the intervals
             if intervals[idx][0] > newInterval[1]:
                 output.append(newInterval)
                 return output + intervals[idx:]
+            #if new interval starts after previous intervals end, append previous, cant return because next idx may change this since its in sorted order
             elif newInterval[0] > intervals[idx][1]:
                 output.append(intervals[idx])       
             else:
