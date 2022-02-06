1b47527bf772fc1fa256b3d29f4fa08f8b2bca48
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-02-05 20:23:58 -0500

    Time: 117 ms (35.66%), Space: 17.3 MB (78.58%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -3,6 +3,6 @@ class Solution:
         intervals.sort()
         
         for i in range(1, len(intervals)):
-            if intervals[i-1][0] < intervals[i][1] and intervals[i][0] < intervals[i-1][1]:
+            if intervals[i][0] < intervals[i-1][1]:
                 return False
         return True
