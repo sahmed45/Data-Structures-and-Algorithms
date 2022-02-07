ef2afd4a7344d00267dc86539625328c3fa61a9b
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-02-06 22:58:26 -0500

    Time: 99 ms (52.62%), Space: 17.5 MB (89.39%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,20 +1,43 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         if not intervals: return 0
+        intervals.sort(key = lambda i: i[0])
+        usedRooms = [intervals[0][1]]
+        heapq.heapify(usedRooms)
         
-        start = sorted([i[0] for i in intervals])
-        end = sorted([i[1] for i in intervals])
-        s, e = 0, 0
-        res, count = 0, 0
+        for interval in intervals[1:]:
+            if interval[0] >= usedRooms[0]:
+                heapq.heappop(usedRooms)
+            heapq.heappush(usedRooms, interval[1])
         
-        while s < len(intervals):
-            #if meeting starts before last one ends
-            if start[s] <  end[e]:
-                s += 1
-                count += 1
-            #if meeting ends before next start
-            else:
-                e += 1
-                count -= 1
-            res = max(count, res)
-        return res
+        return len(usedRooms)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not intervals: return 0
+        
+#         start = sorted([i[0] for i in intervals])
+#         end = sorted([i[1] for i in intervals])
+#         s, e = 0, 0
+#         res, count = 0, 0
+        
+#         while s < len(intervals):
+#             #if meeting starts before last one ends
+#             if start[s] <  end[e]:
+#                 s += 1
+#                 count += 1
+#             #if meeting ends before next start
+#             else:
+#                 e += 1
+#                 count -= 1
+#             res = max(count, res)
+#         return res
