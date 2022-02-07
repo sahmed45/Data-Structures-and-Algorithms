3ba5c47a91ad158422acbcb077ce140ba66628ac
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-02-06 22:34:41 -0500

    Time: 117 ms (39.83%), Space: 17.7 MB (9.61%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,15 +1,18 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         if not intervals: return 0
+        
         start = sorted([i[0] for i in intervals])
         end = sorted([i[1] for i in intervals])
         s, e = 0, 0
         res, count = 0, 0
         
         while s < len(intervals):
+            #if meeting starts before last one ends
             if start[s] <  end[e]:
                 s += 1
                 count += 1
+            #if meeting ends before next start
             else:
                 e += 1
                 count -= 1
