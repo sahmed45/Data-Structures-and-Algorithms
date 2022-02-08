987a33a572ff6e34fd1717bd5cb55134a33eda0a
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-02-07 21:37:22 -0500

    Time: 59 ms (14.51%), Space: 13.8 MB (95.36%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,6 +5,7 @@ class Solution:
         
         for c in s:
             if c in pairs:
+                #if we have closing bracket and last bracket is matching opening bracket
                 if stack and stack[-1] == pairs[c]:
                     stack.pop()
                 else:
