febee74ae05f544df1de3fbf2427a91bb4544120
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-02-07 20:46:14 -0500

    Time: 159 ms (9.95%), Space: 17.4 MB (86.12%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,17 +1,34 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
         output = []
         
         for idx in range(len(intervals)):
-            #if previous intervals start after new one ends, just append new and return rest of the intervals
             if intervals[idx][0] > newInterval[1]:
                 output.append(newInterval)
                 return output + intervals[idx:]
-            #if new interval starts after previous intervals end, append previous, cant return because next idx may change this since its in sorted order
             elif newInterval[0] > intervals[idx][1]:
-                output.append(intervals[idx])       
+                output.append(intervals[idx])
+            #if there is overlap, modify newInterval to merge
             else:
-                newInterval = [min(intervals[idx][0],newInterval[0]), max(intervals[idx][1], newInterval[1])]
-        output.append(newInterval) 
+                newInterval = [min(intervals[idx][0], newInterval[0]),max(intervals[idx][1],newInterval[1])]
+        output.append(newInterval)
         
         return output
+        
+        
+        
+#         for idx in range(len(intervals)):
+#             #if previous intervals start after new one ends, just append new and return rest of the intervals
+#             if intervals[idx][0] > newInterval[1]:
+#                 output.append(newInterval)
+#                 return output + intervals[idx:]
+#             #if new interval starts after previous intervals end, append previous, cant return because next idx may change this since its in sorted order
+#             elif newInterval[0] > intervals[idx][1]:
+#                 output.append(intervals[idx])       
+#             else:
+#                 newInterval = [min(intervals[idx][0],newInterval[0]), max(intervals[idx][1], newInterval[1])]
+#         output.append(newInterval) 
+        
+#         return output
+
