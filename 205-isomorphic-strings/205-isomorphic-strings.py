bd4fcc65c277dc5d82d37e0e09e6021c7b1fd1c8
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-02-07 21:32:20 -0500

    Time: 67 ms (32.96%), Space: 14.3 MB (80.90%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,18 +1,17 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) !=len(t):
+        if len(s) != len(t):
             return False
-        
         mapping = {}
-        reverseMapping = {}
+        reverse_mapping = {}
         
         for i in range(len(s)):
             if s[i] in mapping:
-                if mapping[s[i]] != t[i]: return False
-            elif t[i] in reverseMapping:
-                if reverseMapping[t[i]] != s[i]: return False
-                
-            mapping[s[i]]= t[i]
-            reverseMapping[t[i]]= s[i]
+                if mapping[s[i]] != t[i]:
+                           return False
+            elif t[i] in reverse_mapping:
+                if reverse_mapping[t[i]] != s[i]:
+                           return False
+            mapping[s[i]] = t[i]
+            reverse_mapping[t[i]] = s[i]
         return True
-        
