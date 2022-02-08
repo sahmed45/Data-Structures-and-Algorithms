d2b792f7ebab5261f5af27a36048369081b663a1
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-02-08 18:53:16 -0500

    Time: 68 ms (76.45%), Space: 14.9 MB (88.70%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,37 +1,8 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums_neg = [-n for n in nums]
+        heapq.heapify(nums_neg)
         
-        def quickSelect(nums, k):   
-            pivot = random.choice(nums)
-            greaterThanPivot = [x for x in nums if x > pivot]
-            equalToPivot = [x for x in nums if x == pivot]
-            lessThanPivot = [x for x in nums if x < pivot]
-        
-            greaterLength = len(greaterThanPivot)
-            equalLength = len(equalToPivot)
-        
-            if k <= greaterLength:
-                return quickSelect(greaterThanPivot, k)
-            elif k > greaterLength + equalLength:
-                return quickSelect(lessThanPivot, k - greaterLength - equalLength)
-            else:
-                return equalToPivot[0]
-        return quickSelect(nums, k)
-''''class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        k = len(nums) - k
-        def quickSelect(nums, k):
-
-            pivot = random.choice(nums)
-            if len(nums) < 2:
-                return pivot
-            greaterThanPivot = [x for x in nums if x > pivot]
-            lessThanPivot = [x for x in nums if x <= pivot]
-        
-            greaterLength = len(greaterThanPivot)
-            lesserLength = len(lessThanPivot)
-        
-            if lesserLength > k: return quickSelect(lessThanPivot, k )
-            else: return quickSelect(greaterThanPivot, k - greaterLength )
-
-        return quickSelect(nums, k)'''
+        for _ in range(k-1):
+            heapq.heappop(nums_neg)
+        return -heapq.heappop(nums_neg)
