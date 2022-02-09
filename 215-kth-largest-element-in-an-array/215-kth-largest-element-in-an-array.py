61062655eaf510b7772d3c623de6573782bff5bb
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-02-08 20:06:31 -0500

    Time: 83 ms (59.08%), Space: 14.9 MB (80.06%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,4 +1,5 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        
+        #return last element, nlargest creates an array in descending order
+        #last element would be smallest in that array
         return heapq.nlargest(k, nums)[-1]
