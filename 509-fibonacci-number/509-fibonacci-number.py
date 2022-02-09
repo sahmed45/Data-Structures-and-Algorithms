770680d83b17548f82121d2ae155932b9e4a574c
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-02-08 19:28:03 -0500

    Time: 30 ms (81.89%), Space: 13.8 MB (99.40%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,20 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-#         if n == 0: return 0
-#         if n == 1: return 1
-        
-#         return self.fib(n - 1) + self.fib(n - 2)
-
-        if n <= 1:
+        if n == 0:
             return n
-        
-        #think about fib 2 = fib 1 + fib 0, aka 1 + 0
-        prev1 = 1
-        prev2 = 0
+        res = [0] * (n + 1)
+        res[1] = 1
         
         for i in range(2, n + 1):
-            tmp = prev1
-            prev1 = prev1 + prev2
-            prev2 = tmp
-        return prev1
-            
+            res[i] = res[i-1] + res[i-2]
+        
+        return res[n]
