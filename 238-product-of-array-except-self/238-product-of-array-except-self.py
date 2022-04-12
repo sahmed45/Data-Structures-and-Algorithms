5506cdcba9e4e84b8cf2af0497f8b74ff30713ed
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-04-11 22:56:17 -0400

    Time: 353 ms (34.87%), Space: 21 MB (96.52%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -3,10 +3,11 @@ class Solution:
         length = len(nums)
         prefix = postfix = 1
         ans = [1] * length
-        
+        #update prefix
         for i in range(length):
             ans[i] = prefix
             prefix *= nums[i]
+       #for postfix, update in reverse 
         for i in range(length - 1, -1, -1):
             ans[i] *= postfix
             postfix *= nums[i]
