41e24628bca2df352888b30b9c8bba1f44433484
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-04-19 23:27:07 -0400

    Time: 73 ms (88.04%), Space: 14 MB (69.45%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,58 +1,42 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
 #         if len(s1) > len(s2):
 #             return False
         
-#         s1freq =  [0]*26
-#         s2freq =  [0]*26
+#         s1_freq = s2_freq = [0] * 26
         
 #         for i in range(len(s1)):
-#             s1freq[ord(s1[i])-ord('a')] += 1
-#             s2freq[ord(s2[i])-ord('a')] += 1 
-            
-#         if s1freq == s2freq:
-#             return True
+#             s1_freq[ord(s1[i]) - ord('a')] += 1
+#             s2_freq[ord(s2[i]) - ord('a')] += 1
         
-#         for i in range(len(s1),len(s2)):
-            
-#             s2freq[ord(s2[i])-ord('a')] += 1
-#             s2freq[ord(s2[i-len(s1)])-ord('a')] -= 1
-            
-#             if s1freq == s2freq:
-#                 return True
+#         if s1_freq == s2_freq: return True
         
+#         for i in range(len(s1), len(s2)):
+#             s2_freq[ord(s2[i]) - ord('a')] += 1
+#             s2_freq[ord(s2[i]) - len(s1) - ord('a')] -= 1
+            
+#             if s1_freq == s2_freq: return True
+            
 #         return False
-
-
-        # Check if it is even possible to have a permutation
-        if len(s1) > len(s2):
-            return False
-    
-        count = Counter(s1)
-        # Set up the range for sliding window
-        left, right = 0, len(s1)
+         if len(s1) > len(s2):
+             return False
         
-        # Count the letters for the current sliding window
-        window = Counter(s2[left:right])
+         s1freq =  [0]*26
+         s2freq =  [0]*26
         
-        while right <= len(s2):
-            # Found a permutation
-            if count == window:
-                return True
+         for i in range(len(s1)):
+             s1freq[ord(s1[i])-ord('a')] += 1
+             s2freq[ord(s2[i])-ord('a')] += 1 
             
-            # If key would become 0, delete it.
-            if window[s2[left]] > 1:
-                window[s2[left]] -= 1
-            else:
-                del window[s2[left]]
+         if s1freq == s2freq:
+             return True
+        
+         for i in range(len(s1),len(s2)):
             
-            # Add / Increase the next letter in the string if it exists
-            if right + 1 <= len(s2):
-                window[s2[right]] += 1
+             s2freq[ord(s2[i])-ord('a')] += 1
+             s2freq[ord(s2[i-len(s1)])-ord('a')] -= 1
             
-            # Move the sliding window
-            left += 1
-            right += 1
-            
-        return False
+             if s1freq == s2freq:
+                 return True
+        
+         return False
