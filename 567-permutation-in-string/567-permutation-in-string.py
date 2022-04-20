bfc2be3fce1a07da434bf77a39764cb924560c55
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-04-19 23:36:56 -0400

    Time: 67 ms (93.40%), Space: 13.9 MB (69.45%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,42 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-#         if len(s1) > len(s2):
-#             return False
+         
+        if len(s1) > len(s2): return False
+
+        s1_freq = [0] * 26
+        s2_freq = [0] * 26
+
         
-#         s1_freq = s2_freq = [0] * 26
+        for i in range(len(s1)):
+            s1_freq[ord(s1[i]) - ord('a')] += 1
+            s2_freq[ord(s2[i]) - ord('a')] += 1
         
-#         for i in range(len(s1)):
-#             s1_freq[ord(s1[i]) - ord('a')] += 1
-#             s2_freq[ord(s2[i]) - ord('a')] += 1
+        if s1_freq == s2_freq: 
+            return True
         
-#         if s1_freq == s2_freq: return True
-        
-#         for i in range(len(s1), len(s2)):
-#             s2_freq[ord(s2[i]) - ord('a')] += 1
-#             s2_freq[ord(s2[i]) - len(s1) - ord('a')] -= 1
+        for i in range(len(s1), len(s2)):
+            #increase window(add right char)
+            s2_freq[ord(s2[i]) - ord('a')] += 1
+            #decrease window(remove left char)
+            s2_freq[ord(s2[i - len(s1)]) - ord('a')] -= 1
             
-#             if s1_freq == s2_freq: return True
+            if s1_freq == s2_freq: 
+                return True
             
-#         return False
-         if len(s1) > len(s2):
-             return False
-        
-         s1freq =  [0]*26
-         s2freq =  [0]*26
-        
-         for i in range(len(s1)):
-             s1freq[ord(s1[i])-ord('a')] += 1
-             s2freq[ord(s2[i])-ord('a')] += 1 
-            
-         if s1freq == s2freq:
-             return True
-        
-         for i in range(len(s1),len(s2)):
-            
-             s2freq[ord(s2[i])-ord('a')] += 1
-             s2freq[ord(s2[i-len(s1)])-ord('a')] -= 1
-            
-             if s1freq == s2freq:
-                 return True
-        
-         return False
+        return False
