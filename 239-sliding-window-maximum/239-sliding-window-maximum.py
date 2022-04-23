d5346e983f17443976c3af3319b83cd963e52c02
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-04-22 23:48:30 -0400

    Time: 2223 ms (57.78%), Space: 30.7 MB (19.13%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -6,14 +6,17 @@ class Solution:
         
         while r < len(nums):
             while q and nums[q[-1]] < nums[r]:
+                #pop from right to remove lowest
                 q.pop()
             
             q.append(r)
             
             if l > q[0]:
+                #left index beyond q index, pop from left to remove first
                 q.popleft()
             
             if (r -l + 1) >= k:
+                #so it doesnt append until min window size reached
                 output.append(nums[q[0]])
                 l += 1
             r += 1
