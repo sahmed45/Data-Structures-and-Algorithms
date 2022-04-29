905f6c4ba9eb76ed79cd1c502149eae6934c6482
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-04-29 14:08:09 -0400

    Time: 25 ms (96.61%), Space: 13.9 MB (56.85%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,17 +1,18 @@
 class Solution:
     def fib(self, n: int) -> int:
-#         memo = {}
+        memo = {}
         
-#         def F(i):
-#             if i < 2:
-#                 return i
-#             if i not in memo:
-#                 memo[i] = F(i - 1) + F(i - 2)
-#             return memo[i]
-#         return F(n)
-        
-        F = {}
-        F[0], F[1] = 0, 1 # base cases
-        for i in range(2, n + 1): # topological order
-            F[i] = F[i - 1] + F[i - 2] # relation
-        return F[n] # original
+        def F(i):
+            if i < 2:
+                return i
+            if i not in memo:
+                memo[i] = F(i- 1) + F(i - 2)
+            return memo[i]
+        return F(n)
+
+   #bottom up     
+        # F = {}
+        # F[0], F[1] = 0, 1 # base cases
+        # for i in range(2, n + 1): # topological order
+        #     F[i] = F[i - 1] + F[i - 2] # relation
+        # return F[n] # original
