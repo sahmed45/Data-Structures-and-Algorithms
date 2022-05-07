8b0719c5a59d7bf77983a241eb88d34588c1289a
Syed Ahmed <sahmed45@students.kennesaw.edu>
2022-05-07 00:16:36 -0400

    Time: 863 ms (59.15%), Space: 72.3 MB (31.11%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -10,14 +10,19 @@ class TimeMap:
             self.store[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.store: return ""
         res = ""
-        values = self.store.get(key)
+
+        if key not in self.store: 
+            return res
+        
+        values = self.store[key]
         l = 0
         r = len(values) - 1
         while l <= r:
             mid = (l + r) // 2
-            if values[mid][1] <= timestamp:
+            if values[mid][1] == timestamp:
+                return values[mid][0]
+            if values[mid][1] < timestamp:
                 res = values[mid][0]
                 l = mid + 1
             else:
